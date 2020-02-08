7361565435114b4c99dbd973cb4b042922524bb6
Matheus Lessa <matheus@fastmail.com>
2020-02-08 14:56:20 -0300

    piping tests

--- tests/PipingTests.cs
+++ tests/PipingTests.cs
@@ -6,10 +6,16 @@ public sealed class PipingTests
 	[Theory]
 	[InlineData("1 | assert;", 1)]
 	[InlineData("1 | bypass | assert;", 1)]
-	public void SinglePiping(string source, int expected)
+	[InlineData("1, 2 | assert;", 1, 2)]
+	[InlineData("1, 2, (3 | bypass) | assert;", 1, 2, 3)]
+	public void SimplePiping(string source, params int[] expected)
 	{
+		var expectedValues = new Value[expected.Length];
+		for (var i = 0; i < expectedValues.Length; i++)
+			expectedValues[i] = new Value(expected[i]);
+		var assertCommand = new AssertCommand(expectedValues);
+
 		var engine = new Engine();
-		var assertCommand = new AssertCommand(new Value(expected));
 		engine.RegisterCommand("assert", () => assertCommand);
 		engine.RegisterCommand("bypass", () => new BypassCommand<Tuple0>());
 		source = "extern command assert 0;extern command bypass 0;\n" + source;
